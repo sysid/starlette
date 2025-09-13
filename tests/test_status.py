40a81479c7146bab21cc58ccb92b017cf177a077
Michiel W. Beijen <mb@x14.nl>
2025-09-13 10:30:18 +0200

    feat: implement rfc9110 http status names (#2939)
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>

---
+++ tests/test_status.py
@@ -0,0 +1,23 @@
+import importlib
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "constant,msg",
+    (
+        (
+            "HTTP_413_REQUEST_ENTITY_TOO_LARGE",
+            "'HTTP_413_REQUEST_ENTITY_TOO_LARGE' is deprecated. Use 'HTTP_413_CONTENT_TOO_LARGE' instead.",
+        ),
+        (
+            "HTTP_414_REQUEST_URI_TOO_LONG",
+            "'HTTP_414_REQUEST_URI_TOO_LONG' is deprecated. Use 'HTTP_414_URI_TOO_LONG' instead.",
+        ),
+    ),
+)
+def test_deprecated_types(constant: str, msg: str) -> None:
+    with pytest.warns(DeprecationWarning) as record:
+        getattr(importlib.import_module("starlette.status"), constant)
+        assert len(record) == 1
+        assert msg in str(record.list[0])
